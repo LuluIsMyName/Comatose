800c83dc2fdc2b02762078e99e51bcaecf4dc8e0
LuluIsMyName <07buzz01@gmail.com>
2022-05-08 22:47:57 -0700

    Comatose V1

---
+++ Comatose/Assets/Scripts/AnimatedText.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimatedText : MonoBehaviour
+{
+    /*
+    public Text NarratorText;
+    
+    void Awake()
+    {
+        NarratorText = transform.Find("Narrator").Find("NarratorText").GetComponent<Text>();
+    }
+
+
+    void Start()
+    {
+        NarratorText.text = "hello";
+    }
+    */
+}
