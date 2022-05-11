dc81109332231b166d0b1aa9a660e4b68d5a2b96
LuluIsMyName <07buzz01@gmail.com>
2022-05-10 20:04:41 -0700

    IDP Group 21 Game V2

--- IDP Group 21 Game/Assets/Scripts/MainMenuController.cs
+++ IDP Group 21 Game/Assets/Scripts/MainMenuController.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void PlayGame()
     {
-        
+        SceneManager.LoadScene("Gameplay");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void MainMenu()
     {
-        
+        SceneManager.LoadScene("MainMenu");
     }
 }
