d55eef4a973633ce1a96b6d428338848f6bb5429
LuluIsMyName <07buzz01@gmail.com>
2022-05-11 00:17:24 -0700

    IDP Group 21 Game V3

--- IDP Group 21 Game/Assets/Scripts/MainMenuController.cs
+++ IDP Group 21 Game/Assets/Scripts/MainMenuController.cs
@@ -7,7 +7,7 @@ public class MainMenuController : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("Gameplay");
+        SceneManager.LoadScene("LivingRoom");
     }
 
     public void MainMenu()
